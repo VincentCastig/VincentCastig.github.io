a3ca96172e8ad9cb48c99b5c64d6cd663291e4f9
VincentCastig <vincent.castig@gmail.com>
2018-01-12 21:26:59 -0600

    Joined Index.js to Appropriate __dirname

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const path = require('path');
+const port = 3000
+
+const app = module.exports = express();
+app.use(express.static(path.join(__dirname, './')));
+ //Connects to heroku bro
+
+
+app.listen(port, () => { console.log(`Listening on port: 3000`)});
